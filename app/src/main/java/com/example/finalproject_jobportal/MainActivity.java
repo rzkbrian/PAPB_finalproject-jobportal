8c26878c97ab0d9ffa5b730bc2e452e38b5a3192
Barik Wildani <barikmauluda.wildani@gmail.com>
2023-11-13 00:01:07 +0700

    build login page

--- app/src/main/java/com/example/finalproject_jobportal/MainActivity.java
+++ app/src/main/java/com/example/finalproject_jobportal/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.finalproject_jobportal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +24,18 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnSignin;
     private Button btnSignup;
-
-
+    //Firebase
+    private FirebaseAuth mAuth;
+    //Process Dialog
+    private ProgressDialog mDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mDialog = new ProgressDialog(this);
         loginFunction();
     }
 
@@ -36,6 +51,35 @@ public class MainActivity extends AppCompatActivity {
         btnSignin.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                String mEmail = email.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(mEmail)){
+                    email.setError("Email diperlukan");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(pass)){
+                    password.setError("Password tidak boleh kosong");
+                    return;
+                }
+
+                mDialog.setMessage("Processing");
+                mDialog.show();
+
+                mAuth.createUserWithEmailAndPassword(mEmail,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(), "Successfull", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+
+                            mDialog.dismiss();
+                        } else{
+                            Toast.makeText(getApplicationContext(), "Login Gagal", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
             }
         });
@@ -44,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), RegistrationActivity.class));
             }
+
         });
     }
 }
